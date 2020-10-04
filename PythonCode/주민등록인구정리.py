6ae41ae9c139b440bd57307dbb08eae0cf327012
presentnine <presentnine@naver.com>
2020-10-04 22:19:13 +0900

    구단위 주민등록인구 성별 분류 파일 추가

--- PythonCode/주민등록인구정리.py
+++ PythonCode/주민등록인구정리.py
@@ -8,6 +8,7 @@ inputLivingP="C:\\Users\\admin\\Desktop\\WaRaConv\\DataSet\\주민등록 인구 
 allFilesL=glob.glob(os.path.join(inputLivingP,"서울특별시_주민등록인구및세대현황.csv"))
 dataFrameTotalLp=[]
 
+'''
 #구만 선별
 dataFrameLP=[]
 for file in allFilesL:
@@ -24,5 +25,21 @@ for file in allFilesL:
 dataFrameTotalLpCSV=pd.concat(dataFrameTotalLp,axis=0,ignore_index=True)
 dataFrameTotalLpCSV.to_csv("C:\\Users\\admin\\Desktop\\WaRaConv\\ProcessedDataSet\\서울시 8월 구별 주민등록인구.csv"\
     ,index=False,encoding="cp949")
+'''
 
-print(dataFrameTotalLpCSV['행정동코드'])
+#구 단위 성별 인구
+dFsex=[]
+for file in allFilesL:
+    dataFrameTemp=pd.read_csv(file, encoding='cp949',engine='python')
+    dataFrameTempToTalLP=dataFrameTemp.iloc[1:,[0,4,5]]
+    for i in dataFrameTempToTalLP.index:
+        eachData=dataFrameTempToTalLP.loc[i]
+        infor_list=eachData['행정구역'].split(" ")
+        data={'지역':infor_list[1],'행정동코드':infor_list[2][1:6],'2020년08월_남자 인구수':\
+            eachData['2020년08월_남자 인구수'], '2020년08월_여자 인구수':eachData['2020년08월_여자 인구수'] }
+        dataFrameTotalLp.append(pd.DataFrame(data,\
+            columns=['지역','행정동코드','2020년08월_남자 인구수','2020년08월_여자 인구수'], index=[i]))
+
+dataFrameTotalLpCSV=pd.concat(dataFrameTotalLp,axis=0,ignore_index=True)
+output="C:\\Users\\admin\\Desktop\\WaRaConv\\ProcessedDataSet\\서울시 8월 구별 주민등록인구(성별).csv"
+dataFrameTotalLpCSV.to_csv(output, index=False,encoding='cp949')
