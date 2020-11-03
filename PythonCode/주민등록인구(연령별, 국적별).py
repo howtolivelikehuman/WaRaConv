401cffd51325764ce12673f045e4d7c8ca1ae20b
presentnine <presentnine@naver.com>
2020-11-03 19:43:51 +0900

    구단위 분기 총합 데이터 추가

--- PythonCode/주민등록인구(연령별, 국적별).py
+++ PythonCode/주민등록인구(연령별, 국적별).py
@@ -19,6 +19,27 @@ for file in allFiles:
     
     dfPeople=dfPeople.iloc[:,pos]
 
+    dfAge=[]
+    for i in dfPeople.index:
+        eachData=dfPeople.loc[i]
+        data={'지역':eachData['지역'],
+        '0~9세':(eachData['0~4세']+eachData['5~9세']),
+        '10~19세':(eachData['10~14세']+eachData['15~19세']),
+        '20~29세':(eachData['20~24세']+eachData['25~29세']),
+        '30~39세':(eachData['30~34세']+eachData['35~39세']),
+        '40~49세':(eachData['40~44세']+eachData['45~49세']),
+        '50~59세':(eachData['50~54세']+eachData['55~59세']),
+        '60~69세':(eachData['60~64세']+eachData['65~69세']),
+        '70~79세':(eachData['70~74세']+eachData['75~79세']),
+        '80~89세':(eachData['80~84세']+eachData['85~89세']),
+        '90~99세':(eachData['90~94세']+eachData['95~99세']),
+        }
+        dfAge.append(pd.DataFrame(data,
+        columns=['지역','0~9세','10~19세','20~29세','30~39세','40~49세','50~59세','60~69세',
+        '70~79세','80~89세','90~99세'], index=[i]))
+    
+    dfAge=pd.concat(dfAge,axis=0,ignore_index=True)
+
     outdir="C:\\Users\\admin\\Desktop\\WaRaConv\\ProcessedDataSet\\주민등록인구 연령별 통계\\"
 
-    dfPeople.to_csv(outdir+date+"분기 주민등록인구 연령별 통계.csv",encoding='cp949',index=False)
+    dfAge.to_csv(outdir+date+"분기 주민등록인구 연령별 통계.csv",encoding='cp949',index=False)
--- PythonCode/주민등록인구정리.py
+++ PythonCode/주민등록인구정리.py
@@ -4,28 +4,9 @@ import numpy as np
 import glob
 import os
 
-inputLivingP="C:\\Users\\admin\\Desktop\\WaRaConv\\DataSet\\주민등록 인구 및 세대현황"
-allFilesL=glob.glob(os.path.join(inputLivingP,"서울특별시_주민등록인구및세대현황.csv"))
+inputLivingP="C:\\Users\\admin\\Desktop\\WaRaConv\\DataSet\\주민등록 인구 및 세대현황\\2020"
+allFilesL=glob.glob(os.path.join(inputLivingP,"*서울특별시_주민등록인구및세대현황.csv"))
 dataFrameTotalLp=[]
-
-'''
-#구만 선별
-dataFrameLP=[]
-for file in allFilesL:
-    dataFrameTemp=pd.read_csv(file, encoding='cp949',engine='python')
-    dataFrameTempToTalLP=dataFrameTemp.iloc[1:,[0,1]]
-    for i in dataFrameTempToTalLP.index:
-        eachData=dataFrameTempToTalLP.loc[i]
-        infor_list=eachData['행정구역'].split(" ")
-        data={'지역':infor_list[1],'행정동코드':infor_list[2][1:6],'2020년08월_총인구수':\
-            eachData['2020년08월_총인구수'].replace(',','')}
-        dataFrameTotalLp.append(pd.DataFrame(data,\
-            columns=['지역','행정동코드','2020년08월_총인구수'], index=[i]))
-
-dataFrameTotalLpCSV=pd.concat(dataFrameTotalLp,axis=0,ignore_index=True)
-dataFrameTotalLpCSV.to_csv("C:\\Users\\admin\\Desktop\\WaRaConv\\ProcessedDataSet\\서울시 8월 구별 주민등록인구.csv"\
-    ,index=False,encoding="cp949")
-'''
 
 #구 단위 성별 인구
 dFsex=[]
@@ -35,11 +16,11 @@ for file in allFilesL:
     for i in dataFrameTempToTalLP.index:
         eachData=dataFrameTempToTalLP.loc[i]
         infor_list=eachData['행정구역'].split(" ")
-        data={'지역':infor_list[1],'행정동코드':infor_list[2][1:6],'2020년08월_남자 인구수':\
-            eachData['2020년08월_남자 인구수'], '2020년08월_여자 인구수':eachData['2020년08월_여자 인구수'] }
+        data={'지역':infor_list[1],'행정동코드':infor_list[2][1:6],'남자 인구수':\
+            eachData['2020년08월_남자 인구수'], '여자 인구수':eachData['2020년08월_여자 인구수'] }
         dataFrameTotalLp.append(pd.DataFrame(data,\
-            columns=['지역','행정동코드','2020년08월_남자 인구수','2020년08월_여자 인구수'], index=[i]))
+            columns=['지역','행정동코드','남자 인구수','여자 인구수'], index=[i]))
 
 dataFrameTotalLpCSV=pd.concat(dataFrameTotalLp,axis=0,ignore_index=True)
-output="C:\\Users\\admin\\Desktop\\WaRaConv\\ProcessedDataSet\\서울시 8월 구별 주민등록인구(성별).csv"
+output="C:\\Users\\admin\\Desktop\\WaRaConv\\ProcessedDataSet\\\주민등록인구 성별 통계\\2020 2분기 주민등록인구 성별 통계.csv"
 dataFrameTotalLpCSV.to_csv(output, index=False,encoding='cp949')
